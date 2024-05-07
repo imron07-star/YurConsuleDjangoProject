e4df780546031821a00539d04003df188c218a22
Nurbolottop <erk1nbaevw2711@gmail.com>
2024-05-07 20:54:52 +0600

    fu

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a333+qrzodcgban#k#n5_z0-+xdvc45fr%p8#(ack0cgsh4)=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
